01a9688695f030bf8257f9304d033240d5c87cd7
photondragon <photondragon@163.com>
2024-09-18 17:28:00 +0800

    index.d.ts

--- src/Validation.js
+++ src/Validation.js
@@ -7,26 +7,26 @@ export default class Validation {
   /**
    * 参数验证
    * 此方法主要用于后端API接口的参数验证
-   * @param params 包含待验证参数的Map
-   * @param rules 验证规则。格式示例: [
+   * @param params {object} 包含待验证参数的Map
+   * @param validations {object} 包含验证规则的Map。格式示例: {
    *   "name": "Required|StrLenGeLe:2,50",
    *   "sex": "Required|StrIn:male,female",
    *   "age": "Required|IntGeLe:18,60",
-   *   "height": [
+   *   "height": {
    *     "IfStrEq:male|IntGe:180",
    *     "IfStrEq:female|IntGe:170",
-   *   ]
-   * ]
-   * @param ignoreRequired 是否忽略验证规则中的"Required"验证器
+   *   }
+   * }
+   * @param ignoreRequired {boolean|undefined} 是否忽略验证规则中的"Required"验证器
    */
-  static validate(params, rules, ignoreRequired) {
+  static validate(params, validations, ignoreRequired) {
     if (typeof params !== 'object' || params === null) {
       throw new Error('参数params必须是object');
     }
-    for (let keyPath of Object.keys(rules)) {
-      let validations = rules[keyPath];
+    for (let keyPath of Object.keys(validations)) {
+      let rules = validations[keyPath];
       let keys = this._compileKeyPath(keyPath);
-      this._validate(params, keys, validations, '', ignoreRequired);
+      this._validate(params, keys, rules, '', ignoreRequired);
     }
   }
 
@@ -193,11 +193,11 @@ export default class Validation {
   static #cachedRules = {};
 
   /**
-   *
-   * @param value
-   * @param rules string | string[]
-   * @param alias 如果验证规则中包含'Alias:xxx'，则这个参数可以不传；否则一定要传
-   * @return 返回value原值
+   * 验证一个值是否符合规则
+   * @param value {any} 待验证的值
+   * @param rules {string|string[]} 规则。可以是一条规则；也可以是多条规则的数组，任何一条规则验证通过即可
+   * @param alias {string} 如果验证规则中包含'Alias:xxx'，则这个参数可以不传；否则一定要传
+   * @return {any} 返回value原值
    */
   static validateValue(value, rules, alias) {
     return this._validateValue(value, rules, alias);
@@ -772,10 +772,10 @@ export default class Validation {
 
   /**
    * 设置默认语言
+   * 如果当前语言的翻译表中没有找到翻译项，则会到默认语言的翻译表中查找。
    * 建议在setLang()之前调用
    * 默认语言初始值为'en_US'
-   * 如果当前语言的翻译表中没有找到翻译项，则会到默认语言的翻译表中查找。
-   * @param lang
+   * @param lang {string} 语言代码
    */
   static setDefaultLang(lang) {
     if (!lang || Validation.#defaultLang === lang)
